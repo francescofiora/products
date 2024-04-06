2688e5abc526f29d33010ea78392f7e8213cd3f2
Francesco Fiora <info@francescofiora.it>
2024-04-06 23:53:44 +0100

    Fix Sonar issue

--- product-api/src/main/java/it/francescofiora/product/api/config/InterceptorConfig.java
+++ product-api/src/main/java/it/francescofiora/product/api/config/InterceptorConfig.java
@@ -1,7 +1,7 @@
 package it.francescofiora.product.api.config;
 
 import it.francescofiora.product.api.web.interceptor.EndPointInterceptor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
@@ -12,13 +12,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
  */
 @Profile("Logging")
 @Configuration
-public class InterceptorConfig implements WebMvcConfigurer {
+public class InterceptorConfig {
 
-  @Autowired
-  private EndPointInterceptor endPointInterceptor;
-
-  @Override
-  public void addInterceptors(InterceptorRegistry registry) {
-    registry.addInterceptor(endPointInterceptor);
+  /**
+   * Create WebMvcConfigurer to add EndPointInterceptor.
+   *
+   * @param endPointInterceptor the end-point Interceptor
+   * @return the WebMvcConfigurer
+   */
+  @Bean
+  public WebMvcConfigurer getWebMvcConfigurer(EndPointInterceptor endPointInterceptor) {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(endPointInterceptor);
+      }
+    };
   }
 }
